0cc60fb67c3e35026a6d9ceadf767e340dcc2fe3
AflDavid <93366100+AflDavid@users.noreply.github.com>
2022-01-13 16:53:54 +0200

    Add files via upload

---
+++ Rec3/Rec3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rec3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib.Fibo(0,1,1,5));
+        }
+    }
+}
